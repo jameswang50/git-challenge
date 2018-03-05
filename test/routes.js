77010ac3c8311d8d96c16e053e60ae49db58e794
David Guttman <david@davidguttman.com>
2018-03-05 09:03:35 -0800

    feat: add reverse route

--- test/routes.js
+++ test/routes.js
@@ -9,7 +9,7 @@ var Things = require('../lib/models/things')
 
 tape('healthcheck', function (t) {
   var url = '/health'
-  servertest(server(), url, {encoding: 'json'}, function (err, res) {
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
     t.ifError(err, 'no error')
 
     t.equal(res.statusCode, 200, 'correct statusCode')
@@ -20,7 +20,7 @@ tape('healthcheck', function (t) {
 
 tape('not found', function (t) {
   var url = '/404'
-  servertest(server(), url, {encoding: 'json'}, function (err, res) {
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
     t.ifError(err, 'no error')
 
     t.equal(res.statusCode, 404, 'correct statusCode')
@@ -30,11 +30,11 @@ tape('not found', function (t) {
 })
 
 tape('should get value', function (t) {
-  var val = {some: 'test object'}
+  var val = { some: 'test object' }
   Things.put('test-key', val, function (err) {
     t.ifError(err, 'no error')
     var url = '/things/get/test-key'
-    servertest(server(), url, {encoding: 'json'}, function (err, res) {
+    servertest(server(), url, { encoding: 'json' }, function (err, res) {
       t.ifError(err, 'no error')
 
       t.equal(res.statusCode, 200, 'correct statusCode')
@@ -47,12 +47,12 @@ tape('should get value', function (t) {
 tape('should put values', function (t) {
   var url = '/things/put/test-key2'
   var opts = { method: 'POST', encoding: 'json' }
-  var val = {some: 'other test object'}
+  var val = { some: 'other test object' }
 
   servertest(server(), url, opts, onResponse)
     .end(JSON.stringify(val))
 
-  function onResponse (err, res) {
+  function onResponse(err, res) {
     t.ifError(err, 'no error')
     t.equal(res.statusCode, 200, 'correct statusCode')
 
@@ -84,13 +84,18 @@ tape('should get stream', function (t) {
     })
 })
 
-tape('should get echo', function (t) {
-  var url = '/echo?one=1&two=2'
-  servertest(server(), url, {encoding: 'json'}, function (err, res) {
+tape('should get reverse', function (t) {
+  var expected = {
+    input: 'stringtoreverse',
+    output: 'esreverotgnirts'
+  }
+
+  var url = '/reverse/' + expected.input
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
     t.ifError(err, 'no error')
 
     t.equal(res.statusCode, 200, 'correct statusCode')
-    t.deepEqual(res.body, {one: '1', two: '2'}, 'values should match')
+    t.deepEqual(res.body, expected, 'values should match')
     t.end()
   })
 })
